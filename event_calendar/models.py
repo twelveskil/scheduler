42ef127b65c7a62e16a7923b7fbbcb19c68b54d1
franco <110642088+twelveskil@users.noreply.github.com>
2023-02-07 01:25:28 -0500

    Update models.py

--- event_calendar/models.py
+++ event_calendar/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Event(models.Model):
-    date = models.CharField(max_length=225)
+    date = models.DateTimeField()
     title = models.CharField(max_length=225)
     
     def __str__(self):
         return f"date = {self.date}, title = {self.title}"
+        
