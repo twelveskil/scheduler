7a449c58632024a6403ab8ab4e183a1d26afe67f
franco <110642088+twelveskil@users.noreply.github.com>
2023-02-08 22:27:54 -0500

    Update urls.py

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("event_calendar.urls")),
+    path("calendar/", include("calendar_view.urls")),
 ]
